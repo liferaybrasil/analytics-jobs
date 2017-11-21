418b20b22c509d46088394c88fb6e61fbecce1c2
Inacio Nery <inacio.nery@liferay.com>
2017-11-21 14:20:33 -0300

    Add script to submit the jobs

---
+++ submit_jobs.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+DIR="$( cd "$( dirname "$0" )" && pwd )"
+
+eval "/usr/lib/spark/bin/spark-submit --master spark://192.168.108.90:7077 --deploy-mode cluster --class com.liferay.workflow.labs.spark.Main $DIR/workflow-process-throughput/target/workflow-process-throughput.jar >> $DIR/workflow-process-throughput.log 2>& 1 &"
+eval "/usr/lib/spark/bin/spark-submit --master spark://192.168.108.90:7077 --deploy-mode cluster --class com.liferay.workflow.labs.spark.Main $DIR/workflow-entities/target/workflow-entities.jar >> $DIR/workflow-entities.log 2>& 1 &"
