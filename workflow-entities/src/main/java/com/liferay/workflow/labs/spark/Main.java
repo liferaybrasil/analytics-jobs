8f1d247e74208d3a6621505b2883d7bb061a1a0f
Inacio Nery <inacio.nery@liferay.com>
2017-11-22 14:17:30 -0300

    SF

--- workflow-entities/src/main/java/com/liferay/workflow/labs/spark/Main.java
+++ workflow-entities/src/main/java/com/liferay/workflow/labs/spark/Main.java
@@ -33,47 +33,50 @@ public class Main {
         }
     }
 
-    @SuppressWarnings("serial")
     protected static void doRun() throws AnalysisException {
-        SparkConf conf = new SparkConf();
-        conf.setAppName("Workflow Entities");
-        conf.set("spark.cassandra.connection.host", "192.168.108.90");
-        conf.set("spark.cassandra.auth.username", "cassandra");
-        conf.set("spark.cassandra.auth.password", "cassandra");
+        SparkConf sparkConf = new SparkConf();
+        sparkConf.setAppName("Workflow Entities");
+        sparkConf.set("spark.cassandra.connection.host", "192.168.108.90");
+        sparkConf.set("spark.cassandra.auth.username", "cassandra");
+        sparkConf.set("spark.cassandra.auth.password", "cassandra");
 
-        JavaSparkContext sc = new JavaSparkContext(conf);
+        JavaSparkContext sparkContext = new JavaSparkContext(sparkConf);
 
-        Instant now = Instant.now().minus(5, ChronoUnit.MINUTES);
+        Instant last5Minutes = Instant.now().minus(5, ChronoUnit.MINUTES);
 
-        JavaPairRDD<Long, String> pairs = javaFunctions(sc)
-            .cassandraTable("analytics", "analyticsevent")
-            .select("eventid", "eventproperties")
-            .where("eventid = ? and createdate > ?",
-                "KALEO_DEFINITION_VERSION_CREATE", new Date().from(now))
-            .mapToPair(new PairFunction<CassandraRow, Long, String>() {
+        JavaPairRDD<Long, String> definitionNamePair = javaFunctions(
+            sparkContext).cassandraTable("analytics", "analyticsevent")
+                .select("eventid", "eventproperties")
+                .where("eventid = ? and createdate > ?",
+                    "KALEO_DEFINITION_VERSION_CREATE",
+                    new Date().from(last5Minutes))
+                .mapToPair(new PairFunction<CassandraRow, Long, String>() {
 
-                @Override
-                public Tuple2<Long, String> call(CassandraRow cassandraRow)
-                    throws Exception {
-                    Map<Object, Object> properties = cassandraRow
-                        .getMap("eventproperties");
-                    return new Tuple2<>(Long.valueOf(
-                        (String) properties.get("kaleoDefinitionVersionId")),
-                        (String) properties.get("name"));
-                }
-            });
+                    @Override
+                    public Tuple2<Long, String> call(CassandraRow cassandraRow)
+                        throws Exception {
+                        Map<Object, Object> eventproperties = cassandraRow
+                            .getMap("eventproperties");
+                        return new Tuple2<>(
+                            Long.valueOf((String) eventproperties
+                                .get("kaleoDefinitionVersionId")),
+                            (String) eventproperties.get("name"));
+                    }
+                });
 
-        JavaRDD<Tuple3<String, Long, String>> finalTuple = pairs.map(
-            new Function<Tuple2<Long, String>, Tuple3<String, Long, String>>() {
-                @Override
-                public Tuple3<String, Long, String> call(
-                    Tuple2<Long, String> longStringTuple2) throws Exception {
-                    return new Tuple3<>("KALEO_DEFINITION_VERSION",
-                        longStringTuple2._1(), longStringTuple2._2());
-                }
-            });
+        JavaRDD<Tuple3<String, Long, String>> workflowEntitiesTuple3 = definitionNamePair
+            .map(
+                new Function<Tuple2<Long, String>, Tuple3<String, Long, String>>() {
+                    @Override
+                    public Tuple3<String, Long, String> call(
+                        Tuple2<Long, String> longStringTuple2)
+                        throws Exception {
+                        return new Tuple3<>("KALEO_DEFINITION_VERSION",
+                            longStringTuple2._1(), longStringTuple2._2());
+                    }
+                });
 
-        javaFunctions(finalTuple)
+        javaFunctions(workflowEntitiesTuple3)
             .writerBuilder("analytics", "workflowentities",
                 mapTupleToRow(String.class, Long.class, String.class))
             .withColumnSelector(someColumns("entity", "id", "name"))
