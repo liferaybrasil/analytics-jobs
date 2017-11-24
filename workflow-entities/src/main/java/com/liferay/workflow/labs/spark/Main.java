bbbd7db6a2bbfad07203337f28a42e08892e29c2
Inacio Nery <inacio.nery@liferay.com>
2017-11-24 10:26:05 -0300

    SF

--- workflow-entities/src/main/java/com/liferay/workflow/labs/spark/Main.java
+++ workflow-entities/src/main/java/com/liferay/workflow/labs/spark/Main.java
@@ -7,8 +7,8 @@ import java.time.Instant;
 import java.time.temporal.ChronoUnit;
 import java.util.Date;
 import java.util.HashMap;
+import java.util.Map;
 
-import org.apache.spark.sql.AnalysisException;
 import org.apache.spark.sql.Dataset;
 import org.apache.spark.sql.Row;
 import org.apache.spark.sql.SaveMode;
@@ -16,52 +16,52 @@ import org.apache.spark.sql.SparkSession;
 
 public class Main {
 
-    public static void main(String[] args) {
-        try {
-            doRun();
-        }
-        catch (AnalysisException e) {
-            e.printStackTrace();
-        }
-    }
+	public static void main(String[] args) {
+		doRun();
+	}
 
-    protected static void doRun() throws AnalysisException {
+	protected static void doRun() {
+		SparkSession spark = SparkSession.builder().appName("Workflow Entities")
+			.config("spark.cassandra.connection.host", "192.168.108.90")
+			.config("spark.cassandra.auth.username", "cassandra")
+			.config("spark.cassandra.auth.password", "cassandra").getOrCreate();
 
-        SparkSession spark = SparkSession.builder().appName("Workflow Entities")
-            .config("spark.cassandra.connection.host", "192.168.108.90")
-            .config("spark.cassandra.auth.username", "cassandra")
-            .config("spark.cassandra.auth.password", "cassandra").getOrCreate();
+		Dataset<Row> analyticsEventDataSet = spark.read()
+			.format("org.apache.spark.sql.cassandra")
+			.options(_analyticsEventOptions).load();
 
-        Dataset<Row> analyticsEventDataSet = spark.read()
-            .format("org.apache.spark.sql.cassandra")
-            .options(new HashMap<String, String>() {
-                {
-                    put("keyspace", "analytics");
-                    put("table", "analyticsevent");
-                }
-            }).load();
+		analyticsEventDataSet = analyticsEventDataSet.filter("eventid = '"
+			+ _eventId + "' and createdate > '" + _last5Minutes + "'");
 
-        Instant last5Minutes = Instant.now().minus(5, ChronoUnit.MINUTES);
+		analyticsEventDataSet = analyticsEventDataSet
+			.select(col("eventproperties").getField("kaleoDefinitionVersionId")
+				.as("id"), col("eventproperties").getField("name").as("name"));
 
-        analyticsEventDataSet = analyticsEventDataSet.filter(
-            "eventid = 'KALEO_DEFINITION_VERSION_CREATE' and createdate > '"
-                + new Date().from(last5Minutes) + "'");
+		analyticsEventDataSet = analyticsEventDataSet.withColumn("entity",
+			lit("KALEO_DEFINITION_VERSION"));
 
-        analyticsEventDataSet = analyticsEventDataSet
-            .select(col("eventproperties").getField("kaleoDefinitionVersionId")
-                .as("id"), col("eventproperties").getField("name").as("name"));
+		analyticsEventDataSet.write().format("org.apache.spark.sql.cassandra")
+			.options(_workflowEntitiesOptions).mode(SaveMode.Append).save();
 
-        analyticsEventDataSet = analyticsEventDataSet.withColumn("entity",
-            lit("KALEO_DEFINITION_VERSION"));
+		spark.stop();
+	}
 
-        analyticsEventDataSet.write().format("org.apache.spark.sql.cassandra")
-            .options(new HashMap<String, String>() {
-                {
-                    put("keyspace", "analytics");
-                    put("table", "workflowentities");
-                }
-            }).mode(SaveMode.Append).save();
+	private static final Map<String, String> _analyticsEventOptions = new HashMap<String, String>() {
+		{
+			put("keyspace", "analytics");
+			put("table", "analyticsevent");
+		}
+	};
 
-        spark.stop();
-    }
+	private static final String _eventId = "KALEO_DEFINITION_VERSION_CREATE";
+
+	private static final Date _last5Minutes = Date
+		.from(Instant.now().minus(5, ChronoUnit.MINUTES));
+
+	private static final Map<String, String> _workflowEntitiesOptions = new HashMap<String, String>() {
+		{
+			put("keyspace", "analytics");
+			put("table", "workflowentities");
+		}
+	};
 }
--- workflow-process-throughput/src/main/java/com/liferay/workflow/labs/spark/Main.java
+++ workflow-process-throughput/src/main/java/com/liferay/workflow/labs/spark/Main.java
@@ -8,8 +8,8 @@ import java.time.Instant;
 import java.time.temporal.ChronoUnit;
 import java.util.Date;
 import java.util.HashMap;
+import java.util.Map;
 
-import org.apache.spark.sql.AnalysisException;
 import org.apache.spark.sql.Dataset;
 import org.apache.spark.sql.Row;
 import org.apache.spark.sql.SaveMode;
@@ -17,78 +17,73 @@ import org.apache.spark.sql.SparkSession;
 
 public class Main {
 
-    public static void main(String[] args) {
-        try {
-            doRun();
-        }
-        catch (AnalysisException e) {
-            e.printStackTrace();
-        }
-    }
+	public static void main(String[] args) {
+		doRun();
+	}
 
-    protected static void doRun() throws AnalysisException {
-        SparkSession spark = SparkSession.builder()
-            .appName("Workflow Throughput")
-            .config("spark.cassandra.connection.host", "192.168.108.90")
-            .config("spark.cassandra.auth.username", "cassandra")
-            .config("spark.cassandra.auth.password", "cassandra")
-            .master("local").getOrCreate();
+	protected static void doRun() {
+		SparkSession spark = SparkSession.builder()
+			.appName("Workflow Throughput")
+			.config("spark.cassandra.connection.host", "192.168.108.90")
+			.config("spark.cassandra.auth.username", "cassandra")
+			.config("spark.cassandra.auth.password", "cassandra").getOrCreate();
 
-        Dataset<Row> analyticsEventDataSet = spark.read()
-            .format("org.apache.spark.sql.cassandra")
-            .options(new HashMap<String, String>() {
-                {
-                    put("keyspace", "analytics");
-                    put("table", "analyticsevent");
-                }
-            }).load();
+		Dataset<Row> workflowProcessAvgDataSet = spark.read()
+			.format("org.apache.spark.sql.cassandra")
+			.options(_workflowProcessAvgOptions).load();
 
-        Instant last5Minutes = Instant.now().minus(5, ChronoUnit.MINUTES);
+		Dataset<Row> analyticsEventDataSet = spark.read()
+			.format("org.apache.spark.sql.cassandra")
+			.options(_analyticsEventOptions).load();
 
-        analyticsEventDataSet = analyticsEventDataSet
-            .filter("eventid = 'KALEO_INSTANCE_COMPLETE' and createdate < '"
-                + new Date().from(last5Minutes) + "'");
+		analyticsEventDataSet = analyticsEventDataSet.filter("eventid = '"
+			+ _eventId + "' and createdate > '" + _last5Minutes + "'");
 
-        analyticsEventDataSet = analyticsEventDataSet.select(
-            col("eventproperties").getField("kaleoDefinitionVersionId")
-                .as("kaleodefinitionversionid"),
-            col("eventproperties").getField("duration").as("totalduration"));
+		analyticsEventDataSet = analyticsEventDataSet.select(
+			col("eventproperties").getField("kaleoDefinitionVersionId")
+				.as("kaleodefinitionversionid"),
+			col("eventproperties").getField("duration").as("totalduration"));
 
-        analyticsEventDataSet = analyticsEventDataSet.withColumn("total",
-            lit(1));
+		analyticsEventDataSet = analyticsEventDataSet.withColumn("total",
+			lit(1));
 
-        analyticsEventDataSet = analyticsEventDataSet
-            .select("kaleoDefinitionVersionId", "total", "totalduration");
+		analyticsEventDataSet = analyticsEventDataSet
+			.select("kaleodefinitionversionid", "total", "totalduration");
 
-        Dataset<Row> workflowProcessAvgDataSet = spark.read()
-            .format("org.apache.spark.sql.cassandra")
-            .options(new HashMap<String, String>() {
-                {
-                    put("keyspace", "analytics");
-                    put("table", "workflowprocessavg");
-                }
-            }).load();
+		workflowProcessAvgDataSet = workflowProcessAvgDataSet
+			.select("kaleodefinitionversionid", "total", "totalduration");
 
-        workflowProcessAvgDataSet = workflowProcessAvgDataSet
-            .select("kaleoDefinitionVersionId", "total", "totalduration");
+		workflowProcessAvgDataSet = workflowProcessAvgDataSet
+			.union(analyticsEventDataSet);
 
-        workflowProcessAvgDataSet = workflowProcessAvgDataSet
-            .union(analyticsEventDataSet);
+		workflowProcessAvgDataSet = workflowProcessAvgDataSet
+			.groupBy(col("kaleodefinitionversionid"))
+			.agg(sum("totalduration").as("totalduration"),
+				sum("total").as("total"));
 
-        workflowProcessAvgDataSet = workflowProcessAvgDataSet
-            .groupBy(col("kaleodefinitionversionid"))
-            .agg(sum("totalduration").as("totalduration"),
-                sum("total").as("total"));
+		workflowProcessAvgDataSet.write()
+			.format("org.apache.spark.sql.cassandra")
+			.options(_workflowProcessAvgOptions).mode(SaveMode.Append).save();
 
-        workflowProcessAvgDataSet.write()
-            .format("org.apache.spark.sql.cassandra")
-            .options(new HashMap<String, String>() {
-                {
-                    put("keyspace", "analytics");
-                    put("table", "workflowprocessavg");
-                }
-            }).mode(SaveMode.Append).save();
+		spark.stop();
+	}
 
-        spark.stop();
-    }
+	private static final Map<String, String> _analyticsEventOptions = new HashMap<String, String>() {
+		{
+			put("keyspace", "analytics");
+			put("table", "analyticsevent");
+		}
+	};
+
+	private static final String _eventId = "KALEO_INSTANCE_COMPLETE";
+
+	private static final Date _last5Minutes = Date
+		.from(Instant.now().minus(5, ChronoUnit.MINUTES));
+
+	private static final Map<String, String> _workflowProcessAvgOptions = new HashMap<String, String>() {
+		{
+			put("keyspace", "analytics");
+			put("table", "workflowprocessavg");
+		}
+	};
 }
