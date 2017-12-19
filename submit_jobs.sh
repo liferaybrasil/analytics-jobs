3631b2980ce24bea38463845a875ef84004eab7c
Inacio Nery <inacio.nery@liferay.com>
2017-12-19 09:43:13 -0300

    Update submit jobs

--- submit_jobs.sh
+++ submit_jobs.sh
@@ -2,3 +2,4 @@
 DIR="$( cd "$( dirname "$0" )" && pwd )"
 
 eval "/usr/lib/spark/bin/spark-submit --master spark://192.168.108.90:7077 --deploy-mode cluster --class com.liferay.workflow.labs.spark.Main $DIR/workflow/target/workflow.jar >> $DIR/workflow.log 2>& 1 &"
+eval "/usr/lib/spark/bin/spark-submit --master spark://192.168.108.90:7077 --deploy-mode cluster --class com.liferay.forms.labs.spark.Main $DIR/forms/target/forms.jar >> $DIR/forms.log 2>& 1 &"
